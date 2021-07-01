013dc7bf39745e0fbba968d905e9a6b3d9c69afe
Erick Estrada <erick.e.senado@gmail.com>
2021-07-01 11:05:22 -0500

    creación del archivo que contendra el dashboard y se cambia la vista que sirve la ruta dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// middleware para la autenticación de jetstream
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+    return view('dash.index');
+})->name('dash');
+
+
